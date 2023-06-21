4f58cd96b9075930a7c84a7d60c32fb3d0202951
Sebastian Marines <sebastian0marines@gmail.com>
2023-06-20 21:44:05 -0600

    Enable serviceMonitor for ArgoCD

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -9,6 +9,21 @@ resource "helm_release" "argocd" {
   namespace  = kubernetes_namespace.argocd.metadata[0].name
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "argo-cd"
+
+  set {
+    name  = "controller.metrics.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "controller.metrics.serviceMonitor.namespace"
+    value = kubernetes_namespace.argocd.metadata[0].name
+  }
+
+  set {
+    name  = "controller.metrics.serviceMonitor.enabled"
+    value = "true"
+  }
 }
 
 data "kubernetes_secret" "argocd" {
